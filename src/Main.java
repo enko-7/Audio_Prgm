fb07de7e554928730a0c6d0359b863dd7f33a6fe
enko <c.lauryn@posteo.com>
2025-06-22 17:43:56 +0200

    Kommentare

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        //Scanner Objekt für User input
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Guten Tag, heute helfen wir Ihnen die Ausrichtung des Lautsprechers im freien zu optimieren. ");
 
+
+        //deklarieren der Variablen
         double spl = 0;
         int eingabe = 0;
         boolean gefunden = false;
         boolean check = false;
 
+        // solange die Ausrichtung nicht stimmt wird es wiederholt
         while (!gefunden) {
+
+            //check für User input ob eingabe Valide ist
             while (!check) {
                 try {
                     System.out.println("Geben Sie bitte den Schalldruckpegel in \"dB\" an, wenn man einen Meter entfernt steht:");
@@ -21,10 +27,10 @@ public class Main {
                     check = true;
                 } catch (InputMismatchException e) {
                     System.out.println("Keine Zahl");
-                    sc.next();
+                    sc.nextLine();
                 }
             }
-
+            //check für User input ob eingabe valide ist, anzahl der Werte die angegeben werden
             while (check) {
                 try {
                     System.out.println("Wie viele Distanzen wollen Sie testen? ");
@@ -32,7 +38,7 @@ public class Main {
                     check = false;
                 } catch (InputMismatchException e) {
                     System.out.println("Keine Zahl");
-                    sc.next();
+                    sc.nextLine();
                 }
             }
 
@@ -41,10 +47,11 @@ public class Main {
             double[] ergebnis = new double[eingabe];
 
             double[] werteArr = new double[eingabe];
-            //int y = 0;
 
+            //check für User input ob eingabe valide ist, hier werden die werte eingelsen
             for (int i = 0; i < eingabe; i++) {
-                while (polarPlotWert[polarPlotWert.length-1] == 0) {
+                boolean checkWerte = false;
+                while (!checkWerte) {
                     try {
                         System.out.print(i+1 + ". Eingabe in Meter: ");
                         werteArr[i] = sc.nextDouble();
@@ -54,14 +61,16 @@ public class Main {
                         ergebnis[i] = Math.round((spl + (20 * Math.log10(1.0 / werteArr[i])) - polarPlotWert[i]) * 1000.0) / 1000.0;
 
                         System.out.println(ergebnis[i]);
-                        ++i;
+                        checkWerte =true;
                     } catch (InputMismatchException e) {
                         System.out.println("Keine Zahl");
-                        sc.next();
+                        sc.nextLine();
                     }
                 }
             }
 
+            // sortieren des Arrays damit berechnung durchgeführt werden kann
+        for (int y = 0; y <eingabe-1; y++) {
             for (int i = 0; i < eingabe - 1; i++) {
                 if (ergebnis[i] < ergebnis[i + 1]) {
                     double tmp = ergebnis[i];
@@ -69,6 +78,7 @@ public class Main {
                     ergebnis[i + 1] = tmp;
                 }
             }
+        }
 
 
             System.out.println("Ihre errechneten Werte sortiert, beginnend mit dem größten");
@@ -76,7 +86,7 @@ public class Main {
             for (double j : ergebnis) {
                 System.out.print("[" + j + "]" + " ");
             }
-
+            // hier wird die äußerste while schleife beendet
             if ((ergebnis[0] - ergebnis[ergebnis.length - 1]) <= 6) {
                 gefunden = true;
                 System.out.println("\nIhre Werte liegen im vorgeschlagen Bereich von 6 dB! \n" +
